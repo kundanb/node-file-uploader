d286bb71072a8f893b4787b7e24326d09035a6cd
Kundan Bhasin <kundanbhasin11062000@gmail.com>
2020-11-05 10:47:27 +0530

    setup express

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Welcome to Node File Uploader!');
+const express = require('express');
+const { lookup: dns_lookup } = require('dns');
+const { hostname: os_hostname } = require('os');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, () => {
+  dns_lookup(os_hostname(), (_, addr) => {
+    console.log(`Server started at http://${addr}:${PORT}`);
+  });
+});
